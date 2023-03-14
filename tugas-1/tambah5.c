c9ca52430326cee17dc833ecec0904ceed91e315
b1354 <bayurizkyseptiansyah@gmail.com>
2023-03-14 07:40:52 +0700

    tambah5.c

---
+++ tugas-1/tambah5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    float x;
+    
+    printf("masukan nilai pertama");
+    scanf("%d", &x); //
+
+    x += (5 + 1) * 10;
+    
+    printf("%d", (int)x);
+
+    return 0;
+}
