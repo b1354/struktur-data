9d4ffc3a46cce190243fdee74364c16e8f01dc34
b1354 <bayurizkyseptiansyah@gmail.com>
2023-05-29 09:03:59 +0700

    link_list

--- link_list.c
+++ link_list.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 // List didefinisikan sebagai daftar
 // link didefinisikan sebagai hubungan/kaitan
 // link list didefinisikan secara bebas adalah daftar yang saling terkait
@@ -24,4 +25,33 @@ void pushHead(int dataPush)
     head=current;
   }
   tail->berikut=NULL;
+  printf("data %d berhasil dimasukan\n", dataPush);
+}
+
+void printData() 
+{
+  current = head;
+  // printf("%d\n", current->data);
+  // printf("%d\n", current->berikut);
+  
+  while(current) {
+    printf("%d\n",current->data);
+    current = current->berikut;
+  };
+}
+
+int main() 
+{
+  int input;
+  char ulang = 'y';
+
+  while (ulang == 'y') {
+    printf("masukan bilangan: ");
+    scanf("%d", &input);
+    pushHead(input);
+    printf("\nmasukan lagi(y/n)?");
+    scanf("%s", &ulang);
+  };
+
+  printData();
 }
